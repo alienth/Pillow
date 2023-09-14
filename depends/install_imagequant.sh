f97fd45363cf2d1d5521830d48dfd026eea69355
Jason Harvey <alienth@gmail.com>
2023-09-14 12:52:56 -0800

    yank sudo.

--- depends/install_imagequant.sh
+++ depends/install_imagequant.sh
@@ -6,7 +6,7 @@ git clone -b 2.6.0 https://github.com/pornel/pngquant
 pushd pngquant
 
 make -C lib shared
-sudo cp lib/libimagequant.so* /usr/lib/
-sudo cp lib/libimagequant.h /usr/include/
+cp lib/libimagequant.so* /usr/lib/
+cp lib/libimagequant.h /usr/include/
 
 popd
