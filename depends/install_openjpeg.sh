24957133850cfafa2c38e9d987fa7dbe9799eb33
Jason Harvey <alienth@gmail.com>
2023-09-14 13:01:21 -0800

    Update openjpeg.

--- depends/install_openjpeg.sh
+++ depends/install_openjpeg.sh
@@ -2,16 +2,16 @@
 # install openjpeg
 
 
-if [ ! -f openjpeg-2.1.0.tar.gz ]; then
-    wget -O 'openjpeg-2.1.0.tar.gz' 'https://github.com/python-pillow/pillow-depends/blob/master/openjpeg-2.1.0.tar.gz?raw=true'
+if [ ! -f openjpeg-2.4.0.tar.gz ]; then
+    wget -O 'openjpeg-2.4.0.tar.gz' 'https://github.com/python-pillow/pillow-depends/blob/7a05feeacb1b14918a873c2993ef9bc7815f69c9/openjpeg-2.4.0.tar.gz?raw=true'
 
 fi
 
-rm -r openjpeg-2.1.0
-tar -xvzf openjpeg-2.1.0.tar.gz
+rm -r openjpeg-2.4.0
+tar -xvzf openjpeg-2.4.0.tar.gz
 
 
-pushd openjpeg-2.1.0
+pushd openjpeg-2.4.0
 
 cmake -DCMAKE_INSTALL_PREFIX=/usr . && make -j4 && make -j4 install
 
